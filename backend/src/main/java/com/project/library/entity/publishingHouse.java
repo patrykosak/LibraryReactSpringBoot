1dfe62694a2919ee0e27945a953e471d4170b28e
Patryk Osak <pionchaoo@gmail.com>
2022-02-04 23:43:57 +0100

    add publishing house entity

---
+++ backend/src/main/java/com/project/library/entity/publishingHouse.java
@@ -0,0 +1,32 @@
+package com.project.library.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class publishingHouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long publishingHouseId;
+
+    @NotBlank
+    @Length(max=50)
+    private String name;
+    private String city;
+
+}
