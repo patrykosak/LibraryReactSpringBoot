029355bfab0a13b0e9c2229a6bb5e9a545931564
Patryk Osak <pionchaoo@gmail.com>
2022-02-05 15:15:12 +0100

    add category entity

---
+++ backend/src/main/java/com/project/library/entity/Category.java
@@ -0,0 +1,28 @@
+package com.project.library.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long CategoryId;
+    
+    @NotBlank
+    @Length(max=50)
+    public String name;
+
+}
