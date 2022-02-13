3c67ded0edb7bb4961045abdf3f76a788eab116e
patrykosak <pionchaoo@gmail.com>
2022-02-13 17:21:23 +0100

    add appuser entity

---
+++ backend/src/main/java/com/project/library/entity/AppUser.java
@@ -0,0 +1,35 @@
+package com.project.library.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUser {
+
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1;
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private Long userId;
+    private String name;
+    private String surname;
+    private String username;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+
+}
---
+++ backend/src/main/java/com/project/library/entity/Role.java
@@ -0,0 +1,7 @@
+package com.project.library.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Role {
+}
