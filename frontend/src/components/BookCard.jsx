97eab8cd7e9f70ad20c3479663b26c14a54f69f9
patrykosak <pionchaoo@gmail.com>
2022-02-20 15:05:33 +0100

    book cards improvement

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Card,Button } from 'react-bootstrap'
 
 const BookCard = ({book}) => {
   return (
-    <div>{book.title}</div>
+    <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={book.url} />
+  <Card.Body>
+    <Card.Title>{book.title}</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
   )
 }
 
