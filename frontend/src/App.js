4121a0411f3ad1ce1895d100d64842cb497a655a
patrykosak <pionchaoo@gmail.com>
2022-02-20 13:35:04 +0100

    add main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,17 @@
-import './App.css';
-import AppNavbar from './components/AppNavbar';
-import Footer from './components/Footer';
-import { Route, Routes } from 'react-router-dom';
-import Books from './pages/Books';
-import Layout from './components/Layout';
-
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Books from "./pages/Books";
+import Layout from "./components/Layout";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <Layout>
       <Routes>
+        <Route path="/" element={<MainPage />} />
         <Route path="/books" element={<Books />} />
       </Routes>
-      </Layout>
+    </Layout>
   );
 }
 
