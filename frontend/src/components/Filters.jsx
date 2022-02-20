ab83a997bfff3eef018535ccdb659c438e968044
patrykosak <pionchaoo@gmail.com>
2022-02-20 16:05:19 +0100

    fetching publishing houses filters

--- frontend/src/components/Filters.jsx
+++ frontend/src/components/Filters.jsx
@@ -1,40 +1,41 @@
 import React, { useEffect, useState } from 'react'
 import { Accordion } from 'react-bootstrap'
-import axios, { Axios } from 'axios'
+import axios from 'axios'
 import FilterButton from './FilterButton';
 
 const Filters = () => {
     const [categories, setCategories] = useState([]);
-    const fetchCategories = async () =>{
+    const [publishingHouses, setPublishingHouses] = useState([]);
+    const fetchFilters = async () =>{
         axios.get("http://localhost:8090/categories").then((response)=>{
             setCategories(response.data)
             console.log(categories)
         })
+        axios.get("http://localhost:8090/publishinghouses").then((response)=>{
+            setPublishingHouses(response.data)
+            console.log(publishingHouses)
+        })
     }
     useEffect(()=>{
-        fetchCategories();
+        fetchFilters();
     },[])
   return (
     <>
         <Accordion defaultActiveKey="0" className='m-4'>
   <Accordion.Item eventKey="0">
     <Accordion.Header>Kategorie</Accordion.Header>
-    <Accordion.Body>
+    <Accordion.Body className='d-flex'>
         {categories.map((category,index)=>{
            return <FilterButton name={category.name} index={index} item={category.name}/>
         })}
     </Accordion.Body>
   </Accordion.Item>
   <Accordion.Item eventKey="1">
-    <Accordion.Header>Accordion Item #2</Accordion.Header>
-    <Accordion.Body>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
+    <Accordion.Header>Wydawnictwa</Accordion.Header>
+    <Accordion.Body className='d-flex'>
+        {publishingHouses.map((publisingHouse,index)=>{
+            return <FilterButton name={publisingHouse.name} index={index} item={publisingHouse.name} />
+        })}
     </Accordion.Body>
   </Accordion.Item>
 </Accordion>
