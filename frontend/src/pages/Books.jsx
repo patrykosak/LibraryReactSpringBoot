b4cb3d1be0aac693479ce77d356bd0936bc70e93
patrykosak <pionchaoo@gmail.com>
2022-02-20 14:08:12 +0100

    fetching books

--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
-
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
 const Books = () => {
+    const [books,setBooks] = useState([]);
+    
+    const fetchBooks = async() => {
+        await axios.get("http://localhost:8090/books").then((response)=>{
+            setBooks(response.data)
+            console.log(books)
+        })
+    }
+    useEffect(()=>{
+        fetchBooks()
+    },[])
   return (
     <div>Books</div>
   )
