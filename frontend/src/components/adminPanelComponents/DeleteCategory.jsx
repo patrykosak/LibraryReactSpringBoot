d9336ea5d2c89ea90e41cdb02c3450f2eb45f5c8
patrykosak <pionchaoo@gmail.com>
2022-02-22 23:38:29 +0100

    add delete category feedbacks

--- frontend/src/components/adminPanelComponents/DeleteCategory.jsx
+++ frontend/src/components/adminPanelComponents/DeleteCategory.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Row, Form, FloatingLabel,Button,Col } from 'react-bootstrap'
+import { Row, Form, Alert,Button,Col } from 'react-bootstrap'
 import axios from 'axios'
 import Select from 'react-select';
 
@@ -7,6 +7,7 @@ const DeleteCategory = () => {
     const [disabledButton, setDisabledButton] = useState(true)
     const [categories, setCategories] = useState([])
     const [selectedCategory, setSelectedCategory] = useState([])
+    const [feedback, setFeedback] = useState(null)
     const fetchCategories = async () =>{
 
         axios.get("http://localhost:8090/categories").then((res)=>{
@@ -25,9 +26,32 @@ const DeleteCategory = () => {
 
     const deleteCategory = async (e) =>{
         e.preventDefault()
-        await axios.delete(`http://localhost:8090/categories/${selectedCategory.value}`).then((res)=>console.log(res))
+        await axios.delete(`http://localhost:8090/categories/${selectedCategory.value}`).then((res)=>
+        {
+            console.log(res)
+            if (res.status === 200)
+            setFeedback(
+                <Alert variant="success">
+                    Kategoria została usunięta!
+                </Alert>
+            )
+        else
+            setFeedback(
+                <Alert variant="danger">
+                    Nie udało się usunąć kategorii!
+                </Alert>
+            )
+    }).catch((e) => {
+        console.log(e)
+        setFeedback(
+            <Alert variant="danger">
+                                    Istnieją książki z tej kategorii!
+                    Najpierw usuń te książki
+            </Alert>
+        )
+    })
+}
 
-    }
 
     const selectCategoryHandler = (e) =>{
         console.log(e)
@@ -43,6 +67,7 @@ const DeleteCategory = () => {
                         <Select onChange={(e) => selectCategoryHandler(e)} options={categories} placeholder="Kategoria" />
                     </Col>
                 </Row>
+                {feedback}
                 <div className="d-flex justify-content-end">
                         <Button className="ps-4 pe-4" variant="outline-danger" type="submit" disabled={disabledButton}>
                             Usuń kategorię
