d48ca8260f475c83ff14fd5cd700400505caef81
patrykosak <pionchaoo@gmail.com>
2022-02-26 21:26:51 +0100

    add html validation in book add component

--- frontend/src/components/adminPanelComponents/AddBook.jsx
+++ frontend/src/components/adminPanelComponents/AddBook.jsx
@@ -56,42 +56,42 @@ const AddBook = () => {
         {feedback}
             <Form.Group as={Col} xs={12} md={6} controlId="formGridName">
                 <FloatingLabel controlId="floatingPassword" label="ISBN">
-                    <Form.Control  onChange={(e) => setISBN(e.target.value)} type="text" placeholder="ISBN" required/>
+                    <Form.Control onChange={(e) => setISBN(e.target.value)} minLength={13} maxLength={13} type="text" placeholder="ISBN" required/>
                 </FloatingLabel>
             </Form.Group>
             </Row>
             <Row className="mb-3">
             <Form.Group as={Col} xs={12} md={6} controlId="formGridName">
                 <FloatingLabel controlId="floatingPassword" label="Tytuł">
-                    <Form.Control onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Tytuł" required/>
+                    <Form.Control onChange={(e) => setTitle(e.target.value)} type="text" maxLength={50} placeholder="Tytuł" required/>
                 </FloatingLabel>
             </Form.Group>
             </Row>
             <Row className="mb-3">
             <Form.Group as={Col} xs={12} md={6} controlId="formGridName">
                 <FloatingLabel controlId="floatingPassword" label="Liczba książek">
-                    <Form.Control onChange={(e) => setAmount(e.target.value)}  type="text" placeholder="Rok Liczba książek" required/>
+                    <Form.Control onChange={(e) => setAmount(e.target.value)} min={0} type="number" placeholder="Rok Liczba książek" required/>
                 </FloatingLabel>
             </Form.Group>
         </Row>
         <Row className="mb-3">
             <Form.Group as={Col} xs={12} md={6} controlId="formGridName">
                 <FloatingLabel controlId="floatingPassword" label="link do zdjęcia okładki">
-                    <Form.Control onChange={(e) => setUrl(e.target.value)}  type="text" placeholder="link do zdjęcia okładki" required/>
-                </FloatingLabel>
-            </Form.Group>
-        </Row>
-        <Row className="mb-3">
-            <Form.Group as={Col} xs={12} md={6} controlId="formGridName">
-                <FloatingLabel controlId="floatingPassword" label="Opis książki">
-                    <Form.Control onChange={(e) => setDescription(e.target.value)}  type="text" placeholder="Opis książki" required/>
+                    <Form.Control onChange={(e) => setUrl(e.target.value)}  type="url" placeholder="link do zdjęcia okładki" required/>
                 </FloatingLabel>
             </Form.Group>
         </Row>
         <Row className="mb-3">
             <Form.Group as={Col} xs={12} md={6} controlId="formGridName">
                 <FloatingLabel controlId="floatingPassword" label="Rok wydania">
-                    <Form.Control onChange={(e) => setRelaseYear(e.target.value)}  type="text" placeholder="Rok wydania" required/>
+                    <Form.Control onChange={(e) => setRelaseYear(e.target.value)} min={1800} max={2022} type="number" placeholder="Rok wydania" required/>
+                </FloatingLabel>
+            </Form.Group>
+        </Row>
+        <Row className="mb-3">
+            <Form.Group as={Col} xs={12} md={6} controlId="formGridName">
+                <FloatingLabel controlId="floatingPassword" label="Opis książki">
+                    <Form.Control style={{height: "250px"}} as="textarea" rows={6} onChange={(e) => setDescription(e.target.value)}  type="text" placeholder="Opis książki"/>
                 </FloatingLabel>
             </Form.Group>
         </Row>
