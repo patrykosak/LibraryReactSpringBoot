23aca396318dcabd62d0801c41970fa43f9b8483
Patryk Osak <pionchaoo@gmail.com>
2022-03-01 17:57:04 +0100

    styled login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Form,Button } from 'react-bootstrap'
 
 const Login = () => {
   return (
-    <div className='d-flex justify-content-center'>Login</div>
+    <div style={{minHeight:"60vh",position:"relative"}} className='color-overlay d-flex justify-content-center align-items-center'>
+        
+       
+        <Form style={{border:"3px solid blue"}} className='rounded p-4 p-sm-3'>
+        <h1> Zaloguj się</h1>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email</Form.Label>
+    <Form.Control type="email" placeholder="Email" />
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Hasło</Form.Label>
+    <Form.Control type="password" placeholder="Hasło" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Zaloguj się
+  </Button>
+</Form>
+</div>
   )
 }
 
