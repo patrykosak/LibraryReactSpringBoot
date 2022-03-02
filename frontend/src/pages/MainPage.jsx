620f8267cb035eb66c72ab625a6d0a3a5a9e4af4
Patryk Osak <pionchaoo@gmail.com>
2022-03-02 13:34:09 +0100

    fetching newses on main page

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AppCarousel from '../components/AppCarousel'
 import News from '../components/News'
+import axios from 'axios'
 
 const MainPage = () => {
+  const[newses,setNewses] = useState([])
+
+  const fetchData = async () => {
+    await axios.get("http://localhost:8090/news").then(res=>{
+      setNewses(res.data)
+    })
+  }
+
+  useEffect(()=>{
+    fetchData()
+  },[])
   return (
     <div>
        <AppCarousel />
