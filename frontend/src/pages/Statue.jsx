8b07b35d208c8c54abb23be06f01337cd98e4944
Patryk Osak <pionchaoo@gmail.com>
2022-03-02 23:20:27 +0100

    finished statue page

--- frontend/src/pages/Statue.jsx
+++ frontend/src/pages/Statue.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Statue = () => {
   return (
-    <div>Statue</div>
+    <div style={{minHeight:"75vh"}} className='m-3'>
+        <h1 className='text-center'>Zasady korzystania z biblioteki</h1>
+
+1. Z księgozbioru bibliotecznego mogą korzystać wszyscy uczniowie, rodzice i pracownicy szkoły. <br/>
+2. Wypożyczać książki wolno tylko na swoje nazwisko.<br/>
+3. Dokonując zwrotów książek należy podać swoje nazwisko, imię i klasę.<br/>
+4. Wypożyczeń dla nauczycieli dokonuje się indywidualnie w godzinach pracy biblioteki.<br/>
+5. Ze zbioru podręcznego uczniowie korzystają wyłącznie na miejscu.<br/>
+6. Książki, materiały z tego zbioru mogą być wypożyczane nauczycielom na lekcje. Część księgozbioru można przekazać do użytku w pracowniach przedmiotowych. Wówczas odpowiedzialność materialna za zbiór spoczywa na nauczycielu przedmiotu.<br/>
+7. W celu umożliwienia rodzicom korzystania ze zbiorów bibliotecznych, biblioteka jest czynna podczas zebrań dla rodziców.<br/>
+8. Maksymalny czas wypożyczeń książki to 2 tygodnie. Za zgodą nauczyciela bibliotekarza można go wydłużyć / z wyjątkiem lektur/ na kolejny okres.<br/>
+9. Uczeń może posiadać na swoim koncie jedynie 3 książki /1 lektura /. W wyjątkowych sytuacjach o większej ilości książek decyduje nauczyciel-bibliotekarz.<br/>
+10. Uczeń przygotowujący się do konkursów ma prawo do wypożyczenia większej ilości książek jednorazowo.<br/>
+11. Uczeń, który ma nieoddaną w terminie książkę, nie może korzystać z księgozbioru bibliotecznego.<br/>
+12. Uczniowie szkoły mają prawo przynależności na zasadzie dobrowolności do Koła Bibliotecznego, którego głównym celem jest promowanie postaw czytelniczych.<br/>
+13. Czytelnik zobowiązany jest do szanowania wypożyczonych książek.<br/>
+14. Czytelnik, który zgubi lub zniszczy wypożyczoną książkę zobowiązany jest do niezwłocznego odkupienia takiej samej książki lub innej wskazanej przez nauczyciela-bibliotekarza. Do momentu, gdy tego nie uczyni, jest zawieszony w prawach korzystania z biblioteki.<br/>
+15. Nie wolno samemu brać książek z księgozbioru podręcznego.<br/>
+16. Na dwa tygodnie przed zakończeniem roku szkolnego każdy czytelnik ma obowiązek oddać wszystkie wypożyczone książki.<br/>
+17. W godzinach pracy biblioteki uczniowie mogą korzystać z komputerów i Internetu, w celu poszukiwania informacji do konkursów, projektów i zadań domowych.<br/>
+18 .Nauczyciel bibliotekarz organizując działania współpracuje z Nowohucką Biblioteką Publiczną Filia nr 7- os Kalinowe oraz Pedagogiczną Biblioteką Wojewódzką im. H. Kołłątaja- os Szkolne.<br/>
+19 Każdy czytelnik zobowiązany jest do zaznajomienia się z niniejszym regulaminem i do bezwzględnego przestrzegania go.<br/>
+    </div>
   )
 }
 
