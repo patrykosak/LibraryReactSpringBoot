cca78a6f95b6ed2e09036bedf8f121f4286f871f
Patryk Osak <pionchaoo@gmail.com>
2022-03-02 13:28:26 +0100

    styled delete news component

--- frontend/src/components/adminPanelComponents/DeleteNews.jsx
+++ frontend/src/components/adminPanelComponents/DeleteNews.jsx
@@ -4,8 +4,37 @@ import axios from 'axios'
 import Select from 'react-select'; 
 
 const DeleteNews = () => {
+    const[feedback, setFeedback] = useState([])
+    const[newses, setNewses] = useState([])
+    const[selectedNews, setSelectedNews] = useState(null)
+    const[disabledButton,setDisabledButton] = useState(true)
+
+    const deleteNewsHandler = async (e) => {
+        e.preventDefault();
+
+
+    }
+
+    const selectNewsHandler = (e) => {
+        
+    }
+
   return (
-    <div>DeleteNews</div>
+    <div className="m-3">
+    <Form onSubmit={(e)=> deleteNewsHandler(e)}>
+    <Row className="mb-3">
+        <Col xs={6} md={6}>
+                        <Select onChange={(e) => selectNewsHandler(e)} options={newses} placeholder="Posty" />
+                    </Col>
+                </Row>
+        {feedback}
+        <div className="d-flex justify-content-end">
+            <Button className="ps-4 pe-4" variant="outline-primary" type="submit" disabled={disabledButton}>
+                Usuń post
+            </Button> 
+        </div>
+    </Form>
+</div>
   )
 }
 
