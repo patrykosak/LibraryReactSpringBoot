c65c1e9a70b7a8c36fb73671abd970c39fad5546
Patryk Osak <pionchaoo@gmail.com>
2022-03-02 13:23:20 +0100

    add post delete component

---
+++ frontend/src/components/adminPanelComponents/DeleteNews.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react'
+import { Row, Form, FloatingLabel,Button,Col,Alert } from 'react-bootstrap'
+import axios from 'axios'
+import Select from 'react-select'; 
+
+const DeleteNews = () => {
+  return (
+    <div>DeleteNews</div>
+  )
+}
+
+export default DeleteNews
