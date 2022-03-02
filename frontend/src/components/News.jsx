ab49b3df5d55f7e9683f06df5f314ecb4dbc7782
Patryk Osak <pionchaoo@gmail.com>
2022-03-02 11:34:51 +0100

    add news component

---
+++ frontend/src/components/News.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const News = () => {
+  return (
+    <div className='m-4'>
+    <h1>Tytuł</h1>
+    <hr />
+    <h3>Data</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt mauris non ligula iaculis, at tincidunt enim placerat. Fusce sit amet risus nisi. Integer facilisis imperdiet massa, a rutrum dui laoreet nec. Vestibulum odio nulla, bibendum vel porta a, semper sed eros. Fusce id urna sit amet augue auctor suscipit sed et enim. Quisque ultrices neque sit amet elit consequat, commodo tempor est tincidunt. Quisque rutrum, lectus non pretium interdum, tortor metus pellentesque diam, sit amet tempor ante magna ac tortor.
+
+In vel lorem pharetra, ultricies nibh vitae, porta neque. Suspendisse est nibh, fringilla ac lectus eget, imperdiet aliquet mi. Ut fermentum eu mauris non lobortis. In sit amet hendrerit mi, sed eleifend neque. Aenean dictum sem eu dui fermentum, nec sollicitudin est efficitur. Quisque vel pretium leo. Nunc faucibus metus id imperdiet interdum. Aenean aliquam laoreet libero maximus auctor. Integer lacinia urna at dui pharetra laoreet. Quisque interdum lacus purus, sit amet vehicula ex elementum sed. Quisque commodo ullamcorper mauris. Nunc pharetra nisi augue, eu dapibus metus luctus eget. Nam et elementum justo. Vestibulum id commodo odio, a venenatis nibh. Donec aliquam tortor vel lorem vehicula venenatis.
+
+Phasellus varius metus in eleifend pretium. Nulla ullamcorper ex ac eros commodo, ac mollis nisi suscipit. Praesent ac venenatis lorem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam faucibus dui et ex dignissim rhoncus. Cras non pretium justo. Duis tellus tellus, faucibus a pretium a, feugiat sit amet eros. Pellentesque condimentum pretium sagittis. Vestibulum efficitur urna non nunc ultrices, sit amet faucibus mauris sodales. Nunc sagittis, massa id luctus iaculis, neque lorem euismod metus, at ullamcorper tortor neque vitae magna.
+
+Morbi vitae nibh tortor. Vestibulum aliquam mauris dui, eu eleifend felis porttitor in. Etiam consequat odio non ex laoreet scelerisque. Curabitur porttitor arcu neque. Vivamus sit amet maximus est. Maecenas sit amet suscipit nibh. Vivamus vel tristique sapien, eu molestie leo.
+
+Donec nunc metus, tristique porta porttitor vitae, euismod at arcu. Curabitur eget quam eget felis fringilla pulvinar in ut leo. Morbi pretium nulla eu sem ullamcorper, non aliquet arcu tempor. Phasellus et ex at massa suscipit dignissim. In hac habitasse platea dictumst. Donec rutrum est massa, quis tempus urna suscipit quis. Donec ultricies, enim eu dapibus dignissim, justo lectus pretium sem, quis mattis purus nunc vulputate nibh. Nullam at tortor vel ligula pretium aliquam a quis elit. Maecenas sit amet consequat est, et commodo lectus. Maecenas tempor orci et tellus feugiat molestie. Maecenas felis ipsum, aliquet non leo eu, dapibus bibendum justo.</p>
+    </div>
+  )
+}
+
+export default News
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AppCarousel from '../components/AppCarousel'
+import News from '../components/News'
 
 const MainPage = () => {
   return (
     <div>
        <AppCarousel />
+       <News />
     </div>
   )
 }
