021e2d56e18d48d7ed5785300dd26dbae7a06d1c
Patryk Osak <pionchaoo@gmail.com>
2022-03-06 20:03:37 +0100

    add worker component styled

--- frontend/src/components/adminPanelComponents/EditWorker.jsx
+++ frontend/src/components/adminPanelComponents/EditWorker.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Row, Form, FloatingLabel,Button,Col,Alert } from 'react-bootstrap'
+import axios from 'axios'
+import Select from 'react-select'; 
+
 
 const EditWorker = () => {
+    const[feedback,setFeedback] = useState([]);
+    const[name,setName] = useState("")
+    const[surname,setSurname] = useState("")
+    const[email,setEmail] = useState("")
+    const[workers, setWorkers] = useState([])
+    const[selectedWorker, setselectedWorker] = useState(null)
+    const[disabledButton,setDisabledButton] = useState(true)
+
+    const updateWorkerHandler = async (e) => {
+        e.preventDefault()
+    }
+
+    const selectWorkerHandler = () => {
+
+    }
+
   return (
-    <div>EditWorker</div>
+    <div className="m-3">
+    <Form onSubmit={(e)=> updateWorkerHandler(e)}>
+    <Row className="mb-3">
+            <Form.Group as={Col} xs={12} md={6} controlId="formGridName">
+                    <Select onChange={(e)=>{selectWorkerHandler(e)}} options={workers} placeholder="Pracownik"/>
+            </Form.Group>
+        </Row>
+        {selectedWorker?(<>
+        <Row className="mb-3">
+        {feedback}
+            <Form.Group as={Col} xs={12} md={6} controlId="formGridName">
+                <FloatingLabel controlId="floatingPassword" label="Imię">
+                    <Form.Control value={name} onChange={(e) => {setName(e.target.value);setDisabledButton(false)}} type="text" placeholder="Imię" required/>
+                </FloatingLabel>
+            </Form.Group>
+            </Row>
+            <Row className="mb-3">
+            <Form.Group as={Col} xs={12} md={6} controlId="formGridName">
+                <FloatingLabel controlId="floatingPassword" label="Nazwisko">
+                    <Form.Control value={surname} onChange={(e) => {setSurname(e.target.value);setDisabledButton(false)}} type="text" maxLength={50} placeholder="Nazwisko" required/>
+                </FloatingLabel>
+            </Form.Group>
+            </Row>
+            <Row className="mb-3">
+            <Form.Group as={Col} xs={12} md={6} controlId="formGridName">
+                <FloatingLabel controlId="floatingPassword" label="Email">
+                    <Form.Control value={email} onChange={(e) => {setEmail(e.target.value);setDisabledButton(false)}} min={0} type="email" placeholder="Email" required/>
+                </FloatingLabel>
+            </Form.Group>
+        </Row>
+        <div className="d-flex justify-content-end">
+            <Button className="ps-4 pe-4" variant="outline-primary" type="submit" disabled={disabledButton}>
+                Edytuj pracownika
+            </Button> 
+        </div>
+        </>):null}
+    </Form>
+</div>
   )
 }
 
